3b50887df252454a8a4db260331de075949ef850
natsu-summer72 <yjikxipad@gmail.com>
2019-04-25 19:54:45 +0900

    Change game table from 3x3 to 5x5

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import GameView
+from .views import GameView, WinView, LoseView, DrawView
 
 urlpatterns = [
     path("", GameView.as_view(), name="game"),
+    path("win/", WinView.as_view(), name="win"),
+    path("lose/", LoseView.as_view(), name="lose"),
+    path("draw/", DrawView.as_view(), name="draw"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Table
+from django.views.generic import TemplateView
 
 import json
 import numpy as np
+
+NUM = 25
+
+
+INIT_TABLE = [0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0]
 
 class GameView(View):
     def get(self, request):
@@ -12,7 +22,7 @@ class GameView(View):
         self.params = {}
         """数字->○×の変換"""
         self.num_to_symbol(table)
-        return render(request, "game/game.html", self.params)
+        return render(request, "game/game5.html", self.params)
 
     def post(self, request):
         data = Table.objects.get(data_id=1)
@@ -21,21 +31,25 @@ class GameView(View):
 
         """Clearボタンが押された場合は初期化を行う"""
         if "clear" in request.POST:
-            table = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            table = [0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0]
             data.tb = json.dumps(table)
             data.save()
             """数字->○×の変換"""
             self.num_to_symbol(table)
-            return render(request, "game/game.html", self.params)
+            return render(request, "game/game5.html", self.params)
 
         """ユーザーの入力を反映させる。"""
-        for i in range(9):
+        for i in range(NUM):
             self.params["b"+str(i)] = table[i]
-        for i in range(9):
+        for i in range(NUM):
             if "b"+ str(i) in request.POST:
                 if not table[i] == 0:  #既に選択されているマスの場合
                     self.num_to_symbol(table)
-                    return render(request, "game/game.html", self.params)
+                    return render(request, "game/game5.html", self.params)
                 table[i] = 1
                 self.params["b"+str(i)] = 1
 
@@ -46,7 +60,8 @@ class GameView(View):
             data.save()
             """数字->○×の変換"""
             self.num_to_symbol(table)
-            return render(request, "game/game.html", self.params)
+            return redirect(to="win")
+
 
         """引き分け判定"""
         if self.check_draw(table):
@@ -55,13 +70,13 @@ class GameView(View):
             data.save()
             """数字->○×の変換"""
             self.num_to_symbol(table)
-            return render(request, "game/game.html", self.params)
+            return redirect(to="draw")
 
 
 
         """CPUの入力を反映させる。"""
         while True:
-            action = np.random.randint(0,9)
+            action = np.random.randint(0,NUM)
             if table[action] == 0:
                 break
 
@@ -70,40 +85,128 @@ class GameView(View):
 
         if self.check_win(-1, table):
             self.params["result"] = "Lose"
+            return redirect("lose")
 
         data.tb = json.dumps(table)
         data.save()
 
         self.num_to_symbol(table)
-        return render(request, "game/game.html", self.params)
+        return render(request, "game/game5.html", self.params)
 
     def check_win(self, user, table):
         """勝敗の判定  userは，1がユーザー -1 がCPU"""
-        for i in range(3):
-            if table[3*i]+table[3*i+1]+table[3*i+2]==3*user:  #横
-                return True
-            if table[i]+table[3+i]+table[6+i]==3*user:  #縦
-                return True
-        if table[0]+table[4]+table[8]==3*user:  #斜め(左上から)
-            return True
-        if table[2]+table[4]+table[6]==3*user:  #斜め(右上から)
-            return True
-
+        for k in range(3):  #縦に移動
+            for j in range(3):  #横に移動
+                for i in range(3):
+                    if table[5*i+j+5*k]+table[5*i+j+5*k+1]+table[5*i+j+5*k+2]==3*user:  #横
+                        print("横")
+                        return True
+                    if table[i+j+5*k]+table[5+i+j+5*k]+table[10+i+j+5*k]==3*user:  #縦
+                        print("縦")
+                        return True
+                if table[0+j+5*k]+table[6+j+5*k]+table[12+j+5*k]==3*user:  #斜め(左上から)
+                    print("左ななめ")
+                    return True
+                if table[2+j+5*k]+table[6+j+5*k]+table[10+j+5*k]==3*user:  #斜め(右上から)
+                    print("右ななめ")
+                    return True
         return False
 
     def check_draw(self, table):
         """全てのマスが埋まっているかを判定"""
-        for i in range(9):
+        for i in range(NUM):
             if table[i] == 0:
                 return False
         return True
 
     def num_to_symbol(self, table):
         """render用にtableの数字を記号に変換"""
-        for i in range(9):
+        for i in range(NUM):
             if table[i] == 0:
                 self.params["b"+str(i)] = " "
             elif table[i] == 1:
                 self.params["b"+str(i)] = "○"
             else:
                 self.params["b"+str(i)] = "×"
+        return table
+
+
+class WinView(TemplateView):
+    template_name = "game/result.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["result"] = "You Win"
+        data = Table.objects.get(data_id=1)
+        table = json.loads(data.tb)
+        table = self.num_to_symbol(table)
+        for i in range(NUM):
+            context["b"+str(i)] = table[i]
+        data.tb = json.dumps(INIT_TABLE)
+        data.save()
+        return context
+
+    def num_to_symbol(self, table):
+        """render用にtableの数字を記号に変換"""
+        for i in range(NUM):
+            if table[i] == 0:
+                table[i] = " "
+            elif table[i] == 1:
+                table[i] = "○"
+            else:
+                table[i]= "×"
+
+        return table
+
+class LoseView(TemplateView):
+    template_name = "game/result.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["result"] = "You Lose"
+        data = Table.objects.get(data_id=1)
+        table = json.loads(data.tb)
+        table = self.num_to_symbol(table)
+        for i in range(NUM):
+            context["b"+str(i)] = table[i]
+        data.tb = json.dumps(INIT_TABLE)
+        data.save()
+        return context
+
+    def num_to_symbol(self, table):
+        """render用にtableの数字を記号に変換"""
+        for i in range(NUM):
+            if table[i] == 0:
+                table[i] = " "
+            elif table[i] == 1:
+                table[i] = "○"
+            else:
+                table[i] = "×"
+        return table
+
+
+class DrawView(TemplateView):
+    template_name = "game/result.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["result"] = "Draw"
+        data = Table.objects.get(data_id=1)
+        table = json.loads(data.tb)
+        table = self.num_to_symbol(table)
+        for i in range(NUM):
+            context["b"+str(i)] = table[i]
+        data.tb = json.dumps(INIT_TABLE)
+        data.save()
+        return context
+
+    def num_to_symbol(self, table):
+        """render用にtableの数字を記号に変換"""
+        for i in range(NUM):
+            if table[i] == 0:
+                table[i] = " "
+            elif table[i] == 1:
+                table[i] = "○"
+            else:
+                table[i] = "×"
+        return table
